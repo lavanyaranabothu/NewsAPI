a21dff1cddb2df3a9e8637d8775a42c5e686ec3e
Arun <kiran.arun78@gmail.com>
2024-01-16 14:34:56 +0530

    Everthing_newsapi.py

---
+++ Everthing_newsapi.py
@@ -0,0 +1,16 @@
+import tabulate
+import requests
+def everything():
+    response = requests.get(f'https://newsapi.org/v2/everything?q=bitcoin&apiKey=d66a2c5691fe4db1826283f00030b28e')
+    if response.status_code == 200:
+        output = response.json()
+        outer_list = []
+        for ele in output['articles']:
+            inner_list = (ele['source']['id'],ele['source']['name'], ele['author'],ele['title'],ele['description'],ele['url'],ele['urlToImage'],ele['publishedAt'],ele['content'])
+            outer_list.append(inner_list)
+            print(tabulate.tabulate(outer_list,headers =('id','name','author','title','description','url','urlToImage','publishedAt','content'),tablefmt='simple_grid'))
+    else:
+        print('invalid data')
+
+everything()
+
